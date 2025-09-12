1f100df7cc4921766894fa5ecf267e7ffffc3258
Nay Ba La <naybalaismylife.nbl@gmail.com>
2025-09-12 15:55:51 +0700

    chore : release version 1.0.14

--- templates/controller.js
+++ templates/controller.js
@@ -13,7 +13,7 @@ import * as ${pascalName}Service from "../services";
 export const index = async (req: Request, res: Response): Promise<void> => {
   try {
     const ${repoName}:any = await ${pascalName}Service.index(req);
-    res.successResponse(res, ${repoName}, 200);
+    res.successResponse(${repoName}, ${pascalName} index success);
   } catch (error) {
     res.failResponse(error);
   }
@@ -24,7 +24,7 @@ export const store = async (req: Request, res: Response): Promise<void> => {
     const validated = matchedData(req, { locations: ["body"] });
     if(!req.user?.id) throw new Error('User not found');
     const ${repoName}:any = await ${pascalName}Service.store(validated, req.user.id);
-    res.successResponse(res, ${repoName}, 201);
+    res.successResponse(${repoName}, "${pascalName} create success");
   } catch (error) {
     res.failResponse(error);
   }
@@ -33,7 +33,7 @@ export const store = async (req: Request, res: Response): Promise<void> => {
 export const show = async (req: Request, res: Response): Promise<void> => {
   try {
     const ${repoName}:any = await ${pascalName}Service.show(req.params.id);
-    res.successResponse(res, ${repoName}, 200);
+    res.successResponse(${repoName}, ${pascalName} show success);
   } catch (error) {
     res.failResponse(error);
   }
@@ -44,7 +44,7 @@ export const update = async (req: Request, res: Response): Promise<void> => {
     const validated:any = matchedData(req, { locations: ["body"] });
     if(!req.user?.id) throw new Error('User not found');
     const ${repoName}:any = await ${pascalName}Service.update(validated, req.user.id);
-    res.successResponse(res, ${repoName}, 200);
+    res.successResponse(${repoName}, ${pascalName} update success);
   } catch (error) {
     res.failResponse(error);
   }
@@ -53,7 +53,7 @@ export const update = async (req: Request, res: Response): Promise<void> => {
 export const destroy = async (req: Request, res: Response): Promise<void> => {
   try {
     const deleted:any = await ${pascalName}Service.destroy(req.params.id);
-    res.successResponse(res, deleted, 204);
+    res.successResponse(deleted, ${pascalName} delete success);
   } catch (error) {
     res.failResponse(error);
   }
